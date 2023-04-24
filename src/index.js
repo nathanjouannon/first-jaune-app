040c3eeffa916457e22a21b5e4015820c882af27
Jouannon Nathan <nathan.jouannon@gmail.com>
2023-04-24 16:03:17 +0200

    update

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const typeorm = require('typeorm');
+const Wilders = require("./entity/Wilders");
+
+const app = express();
+
+const dataSource = new typeorm.DataSource({
+  type :'sqlite',
+  database: 'wildersdb/sqlite',
+  synchronize: true,
+  entities :[require('./entity/Wilders')]
+})
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const start = async () => {
+  await dataSource.initialize();
+  dataSource.getRepository(Wilders).save({ name : "frist wilder"})
+  app.listen(3000, () => console.log("Server started on 3000"));
+}
+
+//Start Server
+start()
+
